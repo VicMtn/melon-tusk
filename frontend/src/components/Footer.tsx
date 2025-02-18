9e3d6144866e8716aa45ac9a600f8013ce844865
vicmtn <victorien.montavon@gmail.com>
2025-02-19 00:15:06 +0100

    feat: Create Footer

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+
+const Footer = () => {
+  return (
+    <footer className="footer bottom-0 w-full footer-center  z-50 bg-base-100 py-2">
+      <aside>
+        <p>MelonTusk © 2025 - All right reserved.</p>
+      </aside>
+</footer>
+  );
+}
+
+export default Footer;
