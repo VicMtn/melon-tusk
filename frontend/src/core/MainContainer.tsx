8e9d52c5069404206df7824242ff4c7bb5ec282c
vicmtn <victorien.montavon@gmail.com>
2025-02-19 00:12:51 +0100

    feat: Create MainLayout

---
+++ frontend/src/core/MainContainer.tsx
@@ -0,0 +1,10 @@
+const MainContainer: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return (
+    <div className="sm:ml-64 p-4 h-[calc(100vh-7rem)] bg-orange-50 rounded-lg overflow-auto mt-20">
+      { children } 
+
+    </div>
+  );
+}
+
+export default MainContainer;
