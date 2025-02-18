3668eb970c1606f8f3b9af89d7e2a220afdaf261
vicmtn <victorien.montavon@gmail.com>
2025-02-19 00:19:00 +0100

    feat: Create table in markets page
    LACK: Dynamic data

--- frontend/src/pages/Markets.tsx
+++ frontend/src/pages/Markets.tsx
@@ -1,8 +1,133 @@
 const Markets: React.FC = () => {
   return (
-    <div>
-      <h1>Markets</h1>
+    <>
+    <div className="text-xl font-medium mb-2">
+      Markets
     </div>
+    <div className="border-base-content/25 w-full rounded-lg border">
+    <div className="overflow-x-auto">
+      <table className="table rounded">
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Code</th>
+            <th>Price ($)</th>
+            <th>Volume</th>
+            <th>Market Cap</th>
+            <th className="text-center">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="text-nowrap">#1</td>
+            <td>BTC</td>
+            <td>95'377.13</td>
+            <td><span className="text-sm">14151561000</span></td>
+            <td className="text-sm">March 1, 2024</td>
+            <td className="text-center  gap-2">
+              <button className="btn btn-circle btn-text btn-sm" aria-label="Action button"><span className="icon-[tabler--star] size-5"></span></button>
+              <button className="btn btn-circle btn-success btn-text btn-sm" aria-label="Action button"><span className="icon-[tabler--square-arrow-down] size-5"></span></button>
+              <button className="btn btn-circle btn-error btn-text btn-sm" aria-label="Action button"><span className="icon-[tabler--square-arrow-up] size-5"></span></button>
+              <button className="btn btn-circle btn-text btn-sm" aria-label="Action button"><span className="icon-[tabler--dots-vertical] size-5"></span></button>
+            </td>
+          </tr>
+          <tr>
+            <td className="text-nowrap">#1</td>
+            <td>BTC</td>
+            <td>95'377.13</td>
+            <td><span className="text-sm">14151561000</span></td>
+            <td className="text-sm">March 1, 2024</td>
+            <td className="text-center  gap-2">
+              <button className="btn btn-circle btn-text btn-sm" aria-label="Action button"><span className="icon-[tabler--star] size-5"></span></button>
+              <button className="btn btn-circle btn-success btn-text btn-sm" aria-label="Action button"><span className="icon-[tabler--square-arrow-down] size-5"></span></button>
+              <button className="btn btn-circle btn-error btn-text btn-sm" aria-label="Action button"><span className="icon-[tabler--square-arrow-up] size-5"></span></button>
+              <button className="btn btn-circle btn-text btn-sm" aria-label="Action button"><span className="icon-[tabler--dots-vertical] size-5"></span></button>
+            </td>
+          </tr>
+          <tr>
+            <td className="text-nowrap">#1</td>
+            <td>BTC</td>
+            <td>95'377.13</td>
+            <td><span className="text-sm">14151561000</span></td>
+            <td className="text-sm">March 1, 2024</td>
+            <td className="text-center  gap-2">
+              <button className="btn btn-circle btn-text btn-sm" aria-label="Action button"><span className="icon-[tabler--star] size-5"></span></button>
+              <button className="btn btn-circle btn-success btn-text btn-sm" aria-label="Action button"><span className="icon-[tabler--square-arrow-down] size-5"></span></button>
+              <button className="btn btn-circle btn-error btn-text btn-sm" aria-label="Action button"><span className="icon-[tabler--square-arrow-up] size-5"></span></button>
+              <button className="btn btn-circle btn-text btn-sm" aria-label="Action button"><span className="icon-[tabler--dots-vertical] size-5"></span></button>
+            </td>
+          </tr>
+          <tr>
+            <td className="text-nowrap">#1</td>
+            <td>BTC</td>
+            <td>95'377.13</td>
+            <td><span className="text-sm">14151561000</span></td>
+            <td className="text-sm">March 1, 2024</td>
+            <td className="text-center  gap-2">
+              <button className="btn btn-circle btn-text btn-sm" aria-label="Action button"><span className="icon-[tabler--star] size-5"></span></button>
+              <button className="btn btn-circle btn-success btn-text btn-sm" aria-label="Action button"><span className="icon-[tabler--square-arrow-down] size-5"></span></button>
+              <button className="btn btn-circle btn-error btn-text btn-sm" aria-label="Action button"><span className="icon-[tabler--square-arrow-up] size-5"></span></button>
+              <button className="btn btn-circle btn-text btn-sm" aria-label="Action button"><span className="icon-[tabler--dots-vertical] size-5"></span></button>
+            </td>
+          </tr>
+          <tr>
+            <td className="text-nowrap">#1</td>
+            <td>BTC</td>
+            <td>95'377.13</td>
+            <td><span className="text-sm">14151561000</span></td>
+            <td className="text-sm">March 1, 2024</td>
+            <td className="text-center  gap-2">
+              <button className="btn btn-circle btn-text btn-sm" aria-label="Action button"><span className="icon-[tabler--star] size-5"></span></button>
+              <button className="btn btn-circle btn-success btn-text btn-sm" aria-label="Action button"><span className="icon-[tabler--square-arrow-down] size-5"></span></button>
+              <button className="btn btn-circle btn-error btn-text btn-sm" aria-label="Action button"><span className="icon-[tabler--square-arrow-up] size-5"></span></button>
+              <button className="btn btn-circle btn-text btn-sm" aria-label="Action button"><span className="icon-[tabler--dots-vertical] size-5"></span></button>
+            </td>
+          </tr>
+          <tr>
+            <td className="text-nowrap">#1</td>
+            <td>BTC</td>
+            <td>95'377.13</td>
+            <td><span className="text-sm">14151561000</span></td>
+            <td className="text-sm">March 1, 2024</td>
+            <td className="text-center  gap-2">
+              <button className="btn btn-circle btn-text btn-sm" aria-label="Action button"><span className="icon-[tabler--star] size-5"></span></button>
+              <button className="btn btn-circle btn-success btn-text btn-sm" aria-label="Action button"><span className="icon-[tabler--square-arrow-down] size-5"></span></button>
+              <button className="btn btn-circle btn-error btn-text btn-sm" aria-label="Action button"><span className="icon-[tabler--square-arrow-up] size-5"></span></button>
+              <button className="btn btn-circle btn-text btn-sm" aria-label="Action button"><span className="icon-[tabler--dots-vertical] size-5"></span></button>
+            </td>
+          </tr>
+          <tr>
+            <td className="text-nowrap">#1</td>
+            <td>BTC</td>
+            <td>95'377.13</td>
+            <td><span className="text-sm">14151561000</span></td>
+            <td className="text-sm">March 1, 2024</td>
+            <td className="text-center  gap-2">
+              <button className="btn btn-circle btn-text btn-sm" aria-label="Action button"><span className="icon-[tabler--star] size-5"></span></button>
+              <button className="btn btn-circle btn-success btn-text btn-sm" aria-label="Action button"><span className="icon-[tabler--square-arrow-down] size-5"></span></button>
+              <button className="btn btn-circle btn-error btn-text btn-sm" aria-label="Action button"><span className="icon-[tabler--square-arrow-up] size-5"></span></button>
+              <button className="btn btn-circle btn-text btn-sm" aria-label="Action button"><span className="icon-[tabler--dots-vertical] size-5"></span></button>
+            </td>
+          </tr>
+          <tr>
+            <td className="text-nowrap">#1</td>
+            <td>BTC</td>
+            <td>95'377.13</td>
+            <td><span className="text-sm">14151561000</span></td>
+            <td className="text-sm">March 1, 2024</td>
+            <td className="text-center  gap-2">
+              <button className="btn btn-circle btn-text btn-sm" aria-label="Action button"><span className="icon-[tabler--star] size-5"></span></button>
+              <button className="btn btn-circle btn-success btn-text btn-sm" aria-label="Action button"><span className="icon-[tabler--square-arrow-down] size-5"></span></button>
+              <button className="btn btn-circle btn-error btn-text btn-sm" aria-label="Action button"><span className="icon-[tabler--square-arrow-up] size-5"></span></button>
+              <button className="btn btn-circle btn-text btn-sm" aria-label="Action button"><span className="icon-[tabler--dots-vertical] size-5"></span></button>
+            </td>
+          </tr>
+          
+        </tbody>
+      </table>
+    </div>
+  </div>
+  </>
   );
 };
 
