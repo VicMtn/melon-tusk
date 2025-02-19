fae020957c72a1f0c3d7b4c307cdb24c540a4c08
vicmtn <victorien.montavon@gmail.com>
2025-02-19 19:23:50 +0100

    feat: Add class to the Layout to make it flex

--- frontend/src/core/MainLayout.tsx
+++ frontend/src/core/MainLayout.tsx
@@ -3,10 +3,9 @@ import Navbar from "../components/Navbar";
 import MainContainer from "./MainContainer";
 import Footer from "../components/Footer";
 
-
 const MainLayout: React.FC = () => {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <Navbar />
       <MainContainer>
         <Outlet />
