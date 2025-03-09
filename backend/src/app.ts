c58bb097ae101bebdb812d3a77f006edeaddb562
Minus <pw07vck@eduvaud.ch>
2025-03-09 17:28:06 +0100

    feat: integrate CoinDeskService for news article fetching

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,7 +3,8 @@ dotenv.config();
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
-import { startFetching } from './services/liveCoinWatchService.js'; // Note the .js extension
+import { startFetching } from './services/liveCoinWatchService.js';
+import { CoinDeskService } from './services/CoinDeskService.js';
 
 
 
@@ -18,7 +19,19 @@ mongoose
   .then(() => console.log('Connected to MongoDB: melon-tusk'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
-  startFetching();
+startFetching(); // Start fetching coin data every defined interval
+
+let coinDeskService = new CoinDeskService();
+
+app.get('/news', async (req, res) => {
+  try {
+    const newsArticles = await coinDeskService.fetchNewsArticles();
+    res.json(newsArticles); // Send the news articles as a JSON response
+  } catch (error) {
+    console.error('Error fetching news articles:', error);
+    res.status(500).json({ error: 'Failed to fetch news articles' });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
