70c14b6f20a379ec6bcca50e5f4d4dcc91feb076
vicmtn <victorien.montavon@gmail.com>
2025-03-09 15:02:45 +0100

    fix: User .meta for dotenv with Vite

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -5,13 +5,13 @@ import axios from 'axios';
  */
 const apiClient = axios.create({
 
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
   headers: {
     'Content-Type': 'application/json',
   }
 });
 
-/**
+/** 
  * Interceptor for the responses
  * Only extract the data
  */
