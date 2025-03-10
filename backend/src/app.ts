9a7bc8d5dab80656132c436f8b016d4673b24a9f
Minus <pw07vck@eduvaud.ch>
2025-03-10 08:53:41 +0100

    feat: integrate Swagger UI for API documentation and update LiveCoinWatch service import

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,7 +3,9 @@ dotenv.config();
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
-import { startFetching } from './services/liveCoinWatchService.js'; // Note the .js extension
+import { startFetching } from './services/liveCoinWatchService';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 
 
@@ -19,6 +21,9 @@ mongoose
   .catch((err) => console.error('MongoDB connection error:', err));
 
   startFetching();
+
+const swaggerDocument = YAML.load('src/openapi.yaml');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
