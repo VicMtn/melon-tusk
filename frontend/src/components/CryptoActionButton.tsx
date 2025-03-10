6bd6b364df3a9dc52151f3765fcf31456c0bd016
vicmtn <victorien.montavon@gmail.com>
2025-03-10 16:45:14 +0100

    fix: Render of button in table

--- frontend/src/components/CryptoActionButton.tsx
+++ frontend/src/components/CryptoActionButton.tsx
@@ -15,8 +15,8 @@ const CryptoActionButton: React.FC<CryptoActionButtonProps> = ({
 }) => {
   const isSquare = size === 'sm';
   const buttonClass = `
-    btn 
-    ${action === 'buy' ? 'btn-success' : 'btn-error'} 
+    btn
+    ${action === 'buy' ? 'btn-success' : action === 'sell' ? 'btn-error' : 'btn-text'} 
     ${fullWidth ? 'w-full' : ''} 
     ${size === 'sm' ? 'btn-sm' : size === 'lg' ? 'btn-lg' : ''}
     ${isSquare ? 'btn-square' : ''}
@@ -32,10 +32,15 @@ const CryptoActionButton: React.FC<CryptoActionButtonProps> = ({
           <span className={`icon-[tabler--${isSquare ? 'square-arrow-down' : 'square-arrow-down'}] ${isSquare ? 'size-4' : 'size-5 mr-2'}`}></span>
           {!isSquare && 'Buy'}
         </>
-      ) : (
+      ) : action === 'sell' ? (
         <>
           <span className={`icon-[tabler--${isSquare ? 'square-arrow-up' : 'square-arrow-up'}] ${isSquare ? 'size-4' : 'size-5 mr-2'}`}></span>
           {!isSquare && 'Sell'}
+        </>
+      ) : (
+        <>
+          <span className={`icon-[tabler--${isSquare ? 'star' : 'star'}] ${isSquare ? 'size-4' : 'size-5 mr-2'}`}></span>
+          {!isSquare && 'Watchlist'}
         </>
       )}
     </button>
