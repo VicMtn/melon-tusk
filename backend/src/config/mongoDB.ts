0d84628302bb676fa88227016a54a54181670f77
vicmtn <victorien.montavon@gmail.com>
2025-03-17 10:59:29 +0100

    feat: add MongoDB connection configuration using Mongoose

---
+++ backend/src/config/mongoDB.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import config from './envConfig';
+
+const mongoURI: string = config.mongoUri;
+
+const mongoClient = async (): Promise<void> => {
+  try {
+    await mongoose.connect(mongoURI,)
+    console.log('MongoDB connected successfully');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+  }
+};
+
+export default mongoClient;
